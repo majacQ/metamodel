55411901ef720d2f61e387d94da182db80d6f07f
madprogger <madprogger@web.de>
2014-11-29 16:48:28 +0100

    extend InvocationHelperTest

--- access/src/test/java/metamodel/access/method/InvocationHelperTest.java
+++ access/src/test/java/metamodel/access/method/InvocationHelperTest.java
@@ -23,8 +23,11 @@
  */
 package metamodel.access.method;
 
+import static org.junit.Assert.assertEquals;
 import metamodel.access.Accessor;
+import metamodel.method.Method0;
 import metamodel.method.Method1;
+import metamodel.method.impl.Method0Impl;
 import metamodel.method.impl.Method1Impl;
 
 import org.junit.Test;
@@ -36,17 +39,33 @@ import org.junit.Test;
 public class InvocationHelperTest {
 
 	public static class Target {
-		public void meth(final String text) {
+		private int value;
+
+		@SuppressWarnings("unused")
+		private void meth(final String text) {
 			System.out.println(text);
+		}
+
+		public int getValue() {
+			return value;
+		}
+
+		public void setValue(final int value) {
+			this.value = value;
 		}
 	}
 
 	public static class Target_ {
 		public static Method1<Target, Void, String> meth = new Method1Impl<>("meth", Target.class, String.class);
+		public static Method0<Target, Integer> getValue = new Method0Impl<>("getValue", Target.class);
+		public static Method1<Target, Void, Integer> setValue = new Method1Impl<>("setValue", Target.class, int.class);
 	}
 
 	@Test
 	public void testName() throws Exception {
-		Accessor.on(new Target()).method(Target_.meth).invoke("hello world!!!");
+		final Target target = new Target();
+		assertEquals(null, Accessor.on(target).method(Target_.meth).invoke("hello world!!!"));
+		assertEquals(null, Accessor.on(target).method(Target_.setValue).invoke(42));
+		assertEquals(new Integer(42), Accessor.on(target).method(Target_.getValue).invoke());
 	}
 }
